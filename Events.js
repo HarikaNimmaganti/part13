5db2852ace1bc5765c8c7bac788cce16570dff37
HarikaNimmaganti <116940053+HarikaNimmaganti@users.noreply.github.com>
2023-10-11 16:51:00 +0530

    Add files via upload

---
+++ Events.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Events() {
+  return (
+    <div>
+        <h1>This is Event page</h1>
+         <h3>
+            EventName: Tremendeous Tuesday, Date:03-10-2023, Time: 5:30 to 7:00;<br/>
+            EventName: Freaking Friday, Date:06-10-2023, Time: 5:30 to 7:00;<br/>
+            EventName: Web Development, Date:07-10-2023, Time: 5:30 to 7:00;<br/>
+         </h3>
+    </div>
+  )
+}
